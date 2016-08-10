cf59dfe15c0e57f3dd50f6899cebc6e84264aaae
王玉翠 <m15201386258@163.com>
2016-08-10 14:17:10 +0800

    Initial Commit

---
+++ IOS生成本地验证码/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IOS生成本地验证码
+//
+//  Created by 王玉翠 on 16/8/10.
+//  Copyright © 2016年 王玉翠. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
